561e41a2b014b094f2e940c7e1d3db51b73cba72
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-11 19:18:32 -0300

    Botão de pausar e continuar adicionado

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var minutosHTML = document.getElementById('minutos');
 var segundosHTML = document.getElementById('segundos');
 var tituloPagina = document.querySelector('title');
-var tempoCorrendo = false;
-var intervalo;
 var alarme = document.getElementById('alarme');
+var botaoPause = document.getElementById('botao-pause');
+var tempoCorrendo = false;
+var pausado = false;
+var intervalo;
 
-function minutagem(tempo) {
+function minutagem(minutos,segundos) {
     if (tempoCorrendo) {
         console.log('Espere o tempo acabar');
     } 
@@ -15,53 +17,49 @@ function minutagem(tempo) {
     tempoCorrendo = true;
 
     // Se o tempo for menor do que 10 minutos, adiciono o 0 à esquerda do tempo
-    if (tempo < 10) {
-        minutosHTML.innerText = '0' + tempo;
-        tituloPagina.innerText = '0' + tempo + ':00';
+    if (minutos < 10) {
+        minutosHTML.innerText = '0' + minutos;
+        tituloPagina.innerText = '0' + minutos + ':00';
     } else {
-        minutosHTML.innerText = tempo;
-        tituloPagina.innerText = tempo + ':00';
+        minutosHTML.innerText = minutos;
+        tituloPagina.innerText = minutos + ':00';
     }
 
-    var minutosInt = tempo - 1
-    var segundosInt = 59;
-
     intervalo = setInterval(()=>{
-        if (minutosInt < 10 & segundosInt < 10) {
-            minutosHTML.innerText = '0' + minutosInt;
-            segundosHTML.innerText = '0' + segundosInt;
-            tituloPagina.innerText = '0' + minutosInt + ':' + '0' + segundosInt;
-        } else if (minutosInt < 10) {
-            minutosHTML.innerText = '0' + minutosInt;
-            segundosHTML.innerText = segundosInt;
-            tituloPagina.innerText = '0' + minutosInt + ':' + segundosInt;
-        } else if (segundosInt < 10) {
-            minutosHTML.innerText = minutosInt;
-            segundosHTML.innerText = '0' + segundosInt; 
-            tituloPagina.innerText = minutosInt + ':' + '0' + segundosInt;
+        if (minutos < 10 & segundos < 10) {
+            minutosHTML.innerText = '0' + minutos;
+            segundosHTML.innerText = '0' + segundos;
+            tituloPagina.innerText = '0' + minutos + ':' + '0' + segundos;
+        } else if (minutos < 10) {
+            minutosHTML.innerText = '0' + minutos;
+            segundosHTML.innerText = segundos;
+            tituloPagina.innerText = '0' + minutos + ':' + segundos;
+        } else if (segundos < 10) {
+            minutosHTML.innerText = minutos;
+            segundosHTML.innerText = '0' + segundos; 
+            tituloPagina.innerText = minutos + ':' + '0' + segundos;
         } else {
-            minutosHTML.innerText = minutosInt;
-            segundosHTML.innerText = segundosInt; 
-            tituloPagina.innerText = minutosInt + ':' + segundosInt;
+            minutosHTML.innerText = minutos;
+            segundosHTML.innerText = segundos; 
+            tituloPagina.innerText = minutos + ':' + segundos;
         }
         
-        if (segundosInt == 0){
-            minutosInt -= 1;
-            segundosInt = 59;
+        if (segundos == 0){
+            minutos -= 1;
+            segundos = 59;
         }
 
-        if (minutosInt < 0) {
+        if (minutos < 0) {
             clearInterval(intervalo);
             tempoCorrendo = false;
             tituloPagina.innerText = 'Timer decrescente';
             alarme.play();
         }
 
-        segundosInt -= 1;
+        segundos -= 1;
         
-    }, 1000)}
+    }, 100)}
 }
-
 
 function zerar() {
     clearInterval(intervalo);
@@ -69,4 +67,23 @@ function zerar() {
     segundosHTML.innerText = '00';
     tempoCorrendo = false;
     tituloPagina.innerText = 'Timer decrescente';
+}
+
+function pausarContinuar() {
+    if (pausado) {
+        botaoPause.innerText = 'Pausar';
+        pausado = false;
+        
+        var minutosTranscorridos = parseInt(minutosHTML.innerText);
+        var segundosTranscorridos = parseInt(segundosHTML.innerText);
+
+        minutagem(minutosTranscorridos,segundosTranscorridos);
+        tempoCorrendo = true;
+    }
+    else {
+        clearInterval(intervalo);
+        botaoPause.innerText = 'Continuar';
+        pausado = true;
+        tempoCorrendo = false;
+    }
 }
