3c9a9d5073c8b0bdbc136052523cfc51609f98a9
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-11 19:52:42 -0300

    organização e melhora da coesão

--- app.js
+++ app.js
@@ -7,83 +7,61 @@ var tempoCorrendo = false;
 var pausado = false;
 var intervalo;
 
+function atualizarDisplay(minutos, segundos) {
+    minutosHTML.innerText = minutos < 10 ? '0' + minutos : minutos;
+    segundosHTML.innerText = segundos < 10 ? '0' + segundos : segundos;
+    tituloPagina.innerText = (minutos < 10 ? '0' : '') + minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
+}
+
 function minutagem(minutos,segundos) {
     if (tempoCorrendo) {
         console.log('Espere o tempo acabar');
-    } 
-    
+    }
     else {
     
     tempoCorrendo = true;
-
-    // Se o tempo for menor do que 10 minutos, adiciono o 0 à esquerda do tempo
-    if (minutos < 10) {
-        minutosHTML.innerText = '0' + minutos;
-        tituloPagina.innerText = '0' + minutos + ':00';
-    } else {
-        minutosHTML.innerText = minutos;
-        tituloPagina.innerText = minutos + ':00';
-    }
+    atualizarDisplay(minutos,segundos);
 
     intervalo = setInterval(()=>{
-        if (minutos < 10 & segundos < 10) {
-            minutosHTML.innerText = '0' + minutos;
-            segundosHTML.innerText = '0' + segundos;
-            tituloPagina.innerText = '0' + minutos + ':' + '0' + segundos;
-        } else if (minutos < 10) {
-            minutosHTML.innerText = '0' + minutos;
-            segundosHTML.innerText = segundos;
-            tituloPagina.innerText = '0' + minutos + ':' + segundos;
-        } else if (segundos < 10) {
-            minutosHTML.innerText = minutos;
-            segundosHTML.innerText = '0' + segundos; 
-            tituloPagina.innerText = minutos + ':' + '0' + segundos;
-        } else {
-            minutosHTML.innerText = minutos;
-            segundosHTML.innerText = segundos; 
-            tituloPagina.innerText = minutos + ':' + segundos;
-        }
-        
-        if (segundos == 0){
-            minutos -= 1;
+        if (segundos === 0) {
+            if (minutos === 0) {
+                clearInterval(intervalo);
+                tempoCorrendo = false;
+                tituloPagina.innerText = 'Timer decrescente';
+                alarme.play();
+                return;
+            }
+            minutos--;
             segundos = 59;
+        } else {
+            segundos--;
         }
 
-        if (minutos < 0) {
-            clearInterval(intervalo);
-            tempoCorrendo = false;
-            tituloPagina.innerText = 'Timer decrescente';
-            alarme.play();
-        }
-
-        segundos -= 1;
-        
+        atualizarDisplay(minutos,segundos);        
     }, 1000)}
 }
 
 function zerar() {
     clearInterval(intervalo);
-    minutosHTML.innerText = '00';
-    segundosHTML.innerText = '00';
+    atualizarDisplay(0,0);
     tempoCorrendo = false;
     tituloPagina.innerText = 'Timer decrescente';
 }
 
 function pausarContinuar() {
     if (pausado) {
-        botaoPause.innerText = 'Pausar';
-        pausado = false;
-        
+        tempoCorrendo = false;
         var minutosTranscorridos = parseInt(minutosHTML.innerText);
         var segundosTranscorridos = parseInt(segundosHTML.innerText);
-
+        
         minutagem(minutosTranscorridos,segundosTranscorridos);
+        botaoPause.innerText = 'Pausar';
+        pausado = false;
         tempoCorrendo = true;
     }
-    else {
+    else {        
         clearInterval(intervalo);
         botaoPause.innerText = 'Continuar';
         pausado = true;
-        tempoCorrendo = false;
     }
 }
