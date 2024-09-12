559b594af8264a6019dffecdd4b86302c7455118
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-12 11:43:53 -0300

    Função de tela cheia adicionada

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+var divTempo = document.getElementById('tempo');
 var minutosHTML = document.getElementById('minutos');
 var segundosHTML = document.getElementById('segundos');
 var tituloPagina = document.querySelector('title');
 var alarme = document.getElementById('alarme');
 var botaoPause = document.getElementById('botao-pause');
+var botaoFullscreen = document.getElementById('tela-cheia');
 var tempoCorrendo = false;
 var pausado = false;
 var intervalo;
@@ -71,3 +73,34 @@ function pausarContinuar() {
         pausado = true;
     }
 }
+
+function telaCheia() {
+    if (!document.fullscreenElement) {
+        divTempo.classList.add('fullscreen-mode');
+        if (divTempo.requestFullscreen) {
+            divTempo.requestFullscreen();
+        } else if (divTempo.mozRequestFullScreen) { // Para Firefox
+            divTempo.mozRequestFullScreen();
+        } else if (divTempo.webkitRequestFullscreen) { // Para Chrome, Safari e Opera
+            divTempo.webkitRequestFullscreen();
+        } else if (divTempo.msRequestFullscreen) { // Para IE/Edge
+            divTempo.msRequestFullscreen();
+        }
+
+        if (screen.orientation && screen.orientation.lock) {
+            screen.orientation.lock('landscape').catch(err => {
+                console.log("Falha ao mudar para o modo paisagem", err);
+            });
+        }
+    } else {
+        if (document.exitFullscreen) {
+            divTempo.classList.remove('fullscreen-mode');
+        }
+    }
+}
+
+document.addEventListener('fullscreenchange', () => {
+    if (!document.fullscreenElement) {
+        divTempo.classList.remove('fullscreen-mode');
+    }
+});
